132e7aec03c19f4d30cfc672777e8e94b6822855
oyebanji_yusuf <oyebanjiyusuf3@gmail.com>
2019-10-30 02:04:28 +0100

    edit profile

--- editprofile.php
+++ editprofile.php
@@ -4,7 +4,7 @@
 if (isset($_POST['delete'])) 
                   {
                         $con=$_POST['delete'];
-                        $sql = "DELETE FROM users WHERE matric='$con'";
+                        $sql = "DELETE FROM aspirants WHERE staff_id='$con'";
                         $retval = mysqli_query( $db,$sql );
                     if(! $retval )
                             {
@@ -21,9 +21,9 @@ if (isset($_POST['update']))
                   echo "<style type='text/css'>
                   .first{display:none; }
                 </style>";
-                $matric=$_POST['update'];
-            if ($matric) {
-              $_SESSION['user_profile'] = $matric;
+                $staff_id=$_POST['update'];
+            if ($staff_id) {
+              $_SESSION['user_profile'] = $staff_id;
               header("location:updateprofile.php");
             }
             }
@@ -32,20 +32,22 @@ if (isset($_POST['update']))
   <div class="row justify-content-center">
     <div class="col-lg-9 text-capitalize">
       <?php
-      $department=department($_SESSION['student_user']);
-$sql="SELECT * FROM users WHERE department ='$department'";
+      // $department=department($_SESSION['student_user']);
+// $staff_id=$_SESSION(['staff_id']);
+$staff_id=$_SESSION['staff_id'];
+$sql="SELECT * FROM aspirants WHERE staff_id ='$staff_id'";
 $query=mysqli_query($db,$sql);
-echo "<table class='table table-hover bg-light ' style='margin-top:-100px; '><form method='POST'>";
+echo "<table class='table table-hover bg-light ' style='margin-top:50px; '><form method='POST'>";
 echo " <thead>
     <tr>
- <th class='alert-success text-center text-uppercase' colspan='7'><b> User Profile for ".$department."</b></th>
+ <th class='alert-success text-center text-uppercase' colspan='7'><b> User Profile for ".$staff_id."</b></th>
     </tr>
-    <tr class='alert-light text-dark'><td><b>Sn</b></td><td><b>Surname</b></td><td><b>Name</b></td><td><b>Matric</b></td><td><b>Sex</b></td><td><i class='fa fa-trash'></i></td><td><i class='fa fa-refresh'></i></td></b></tr>
+    <tr class='alert-light text-dark'><td><b>Sn</b></td><td><b>Surname</b></td><td><b>Name</b></td><td><b>Staff ID</b></td><td><b>Sex</b></td><td><i class='fa fa-trash'></i></td><td><i class='fa fa-refresh'></i></td></b></tr>
   </thead>";{$count=1;
 while ($result=mysqli_fetch_array($query)) 
-echo"<tbody><tr>"."<td>".$count++."</td>"."<td>" .$result['surname']."</td>"."<td class=''>" .$result['name']."</td>"."<td>" .$result['matric']."</td>"."<td class=''>" .$result['sex']."</td>".
-"<td><button type='submit' name='delete' value='".$result['matric']."' class='btn btn-sm btn-danger '>DELETE</button></td>".
-"<td><button type='submit' name='update' value='".$result['matric']."' class='btn btn-sm bg-success' style='color:#fff;' >UPDATE</button></td>"."</tr><br>";
+echo"<tbody><tr>"."<td>".$count++."</td>"."<td>" .$result['surname']."</td>"."<td class=''>" .$result['name']."</td>"."<td>" .$result['staff_id']."</td>"."<td class=''>" .$result['sex']."</td>".
+"<td><button type='submit' name='delete' value='".$result['staff_id']."' class='btn btn-sm btn-danger '>DELETE</button></td>".
+"<td><button type='submit' name='update' value='".$result['staff_id']."' class='btn btn-sm bg-success' style='color:#fff;' >UPDATE</button></td>"."</tr><br>";
 }
 echo "</tbody></form></table>";
 ?>
